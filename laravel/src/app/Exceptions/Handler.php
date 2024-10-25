2fb0d8787bb088b082c9b32af7c307d5b7d5e94a
n43 <49.3@laposte.net>
2024-10-25 15:29:37 +0200

    sonarqube added

---
+++ laravel/src/app/Exceptions/Handler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = ["current_password", "password", "password_confirmation"];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json([
+                'status' => 'not-found',
+                'message' => 'Not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($e instanceof ValidationException) {
+            return response()->json([
+                'status' => 'invalid',
+                'message' => 'Validation failed',
+                'validation_errors' => $e->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        return parent::render($request, $e);
+    }
+}
